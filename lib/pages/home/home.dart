90189e449972bc5022c12d7bc6fa61e79c02a255
Harilanto <raharisonhfanambinantsoa@gmail.com>
2023-09-25 19:23:17 +0300

    style image transform and positioned

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -10,7 +10,27 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView( //widget qui nous permet de scroller du contenu
         child: Stack( //nous permet de supperposer différents éléments
           children: [
-            Image.asset('assets/images/bg_liquid.png'),
+            Transform( //permet de faire tourner ou déplacer
+              transform: Matrix4.identity()..rotateZ(20),
+              origin: const Offset(150, 50),//décalage en fonction de l'axe vertical et horizontal
+              child: Image.asset(
+                'assets/images/bg_liquid.png',
+                width: 200,
+              ),
+            ),
+            Positioned(//va permettre dans un widget stack de positionner de manière absolu les différents enfants
+              right: 0,
+              top: 200,
+              child: Transform(
+                transform: Matrix4.identity()..rotateZ(20),
+                origin: const Offset(180, 100),
+                child: Image.asset(
+                  'assets/images/bg_liquid.png',
+                  width: 200,
+                ),
+              ),
+            ),
+            Container()
           ],
           )
       ),
